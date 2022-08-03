faf9cda5bd1154a80d759a2c02288820f244cadd
Lyall <58636255+lyall-pc@users.noreply.github.com>
2022-08-03 09:02:47 +0100

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -5,7 +5,7 @@ if exist "node_modules" (
     timeout /t 5 /nobreak
 ) else (
     echo First run, installing modules...
-    npm i @ffmpeg-installer/ffmpeg@1.1.0 @ffprobe-intsaller/ffprobe fluent-ffmpeg@2.1.2 prompts@2.4.2
+    npm i @ffmpeg-installer/ffmpeg@1.1.0 @ffprobe-installer/ffprobe fluent-ffmpeg@2.1.2 prompts@2.4.2
     echo Finished installing modules!
     title = Video to Image Converter by Lyall  
     node ./src/index.js
